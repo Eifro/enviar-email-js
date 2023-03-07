d5de939116f9facbeb37a91e3030a1ed19e52bfe
eifro <afrotricks19@gmail.com>
2023-03-07 17:38:15 -0500

    validar email

--- js/app.js
+++ js/app.js
@@ -24,16 +24,41 @@ document.addEventListener('DOMContentLoaded', function() {
     /* funciones */
     function validarInput(e)
     {
+        limpiarAlerta(e.target.parentElement) // ejecuta cuando hay texto en el campo
+
         if (e.target.value.trim() === '') { // trim, borra los espacios de ambos lados
-            if (!e.target.parentElement.lastElementChild.classList.contains('alerta')) mostrarAlerta(`Falta completar el campo ${e.target.id}`, e.target.parentElement) // validar si es que hay una alerta
+            mostrarAlerta(`Falta completar el campo ${e.target.id}`, e.target.parentElement)
+            return // detiene la ejecución del código
         }
+        
+        if (!validarEmail(e.target.value)) {
+            mostrarAlerta('El email no es válido', e.target.parentElement)
+            return
+        }
+
+        
     }
     
     function mostrarAlerta(msg, referencia)
     {
-        const error = document.createElement('P')
-        error.textContent = msg
-        error.className = 'bg-red-600 p-2 text-white text-center font-bold alerta'
-        referencia.appendChild(error) // agrega un nuevo elemento debajo de cada input
+        if (!referencia.lastElementChild.classList.contains('alerta')) {
+            const error = document.createElement('P')
+            error.textContent = msg
+            error.className = 'bg-red-600 p-2 text-white text-center font-bold alerta'
+            referencia.appendChild(error) // agrega un nuevo elemento debajo de cada input
+        }
+    }
+
+    function limpiarAlerta(referencia)
+    {
+        const alerta = referencia.querySelector('.alerta')
+        if (alerta) alerta.remove()
+    }
+
+    function validarEmail(email)
+    {
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+        const isEmail = regex.test(email)
+        return isEmail
     }
 })
